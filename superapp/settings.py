9036961ff04a7a27ca9816dd38ee045ef1f00206
Pavel Skobelev <pskobelev@me.com>
2024-04-10 00:36:07 +0300

    Add logging configuration to Django settings for improved debugging

--- superapp/settings.py
+++ superapp/settings.py
@@ -119,3 +119,19 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import os
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
